b07d68ade44bcba51b091ff7330a2a06b71c8e70
Ael <bluebayband@gmail.com>
2022-08-14 21:34:33 -0700

    save user stuff

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -13,10 +13,30 @@ import { StyleSheet,
 export default function App() {
   // change between work and travel
   const [working, setWorking] = useState(true);
+  // save user input
+  const [text, setText] = useState("");
+  // save toDos
+  const [toDos, setToDos] = useState({});
 
   // switch between modes
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+
+  const onChangeText = (event) => {
+    setText(event);
+  }
+
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    } else {
+      // save to do
+      setToDos((currentList) => currentList[Date.now()] = text);
+      console.log(toDos);
+    }
+    // empty text input box 
+    setText("");
+  }
 
   return (
     <View style={styles.container}>
@@ -29,11 +49,13 @@ export default function App() {
           <Text style={{...styles.buttonText, color: !working ? "white" : theme.gray}}>Travel</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.input}>
-            <TextInput 
-              placeholder={working ? "Add a To Do" : "Where do you want to go?"}
-            />
-        </View>
+      <TextInput 
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+        returnKeyType="done"
+        onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
+      />
     </View>
   );
 }
@@ -55,5 +77,10 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizonal: 10,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   }
 });
